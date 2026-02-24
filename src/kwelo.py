631f243d30bbc593a23eff6fcfef3f7b75fcb373
huok <77536370+vraestoren@users.noreply.github.com>
2026-02-24 15:23:59 +0300

    Update kwelo.py

--- src/kwelo.py
+++ src/kwelo.py
@@ -1,13 +1,14 @@
 from os import getcwd
 from time import time
 from pathlib import Path
-from requests import get
+from requests import Session
 
 class Kwelo:
 	def __init__(self) -> None:
 		self.api = "https://api.kwelo.com/v1"
-		self.headers = {
-			"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
+		self.session = Session()
+		self.session.headers = {
+			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
 		}
 
 	def save_file(
@@ -15,24 +16,19 @@ class Kwelo:
 			content: bytes,
 			location: str = getcwd()) -> bool:
 		with open(
-			Path(location).joinpath(f"{time() * 1000}.jpg"),
-		mode="wb+",
-		) as file:
-			file.write(content)
-			file.close()
+			Path(location).joinpath(f"{time() * 1000}.jpg"), mode="wb+") as file:
+				file.write(content)
+				file.close()
 		return True
 
 	def get_self_ip_address(self) -> dict:
-		return get(
-			f"{self.api}/network/ip-address/my?format=json",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/network/ip-address/my?format=json").json()
 
 	def get_ip_address_location(self, ip_address: str) -> dict:
-		return get(
-			f"{self.api}/network/ip-address/location/{ip_address}?format=json",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/network/ip-address/location/{ip_address}?format=json").json()
 
 	def get_identicon(self, name: str) -> bool:
-		return self.save_file(get(
-			f"{self.api}/media/identicon/{name}",
-			headers=self.headers).content)
+		return self.save_file(self.session.get(
+			f"{self.api}/media/identicon/{name}").content)
